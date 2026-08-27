bf5c62e990ead5eba1fc6ca05740a9739009eae1
Andrescjmaster <141934770+Andrescjmaster@users.noreply.github.com>
2026-08-26 23:57:12 -0500

    Update conexion.php to simulate DB connection
    Temporarily simulate database connection for deployment.

--- Alquimia/conexion.php
+++ Alquimia/conexion.php
@@ -1,13 +1,18 @@
 <?php
 $host = "localhost";
 $usuario = "root";
-$password = ""; // en XAMPP normalmente va vacío
+$password = ""; 
 $bd = "alquimia_db";
 
+// Conexión simulada temporalmente para despliegue
+$conn = null;
+
+/*
 $conn = new mysqli($host, $usuario, $password, $bd);
 
 // Verificar conexión
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
+*/
 ?>
